dfcc03e2123e363b0d8e4544e32b3badd155f843
adonu <adonu@DESKTOP-2DU7K8D>
2024-08-24 01:05:40 -0400

    Agregar archivos de proyecto.

---
+++ Practica/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Practica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            String a = "Hola";
+
+            Console.WriteLine($"Wena {a}");
+        }
+    }
+}
