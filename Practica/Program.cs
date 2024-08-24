caab4171653b9b1279e79e414aa608f094f48eeb
sinoda4 <sinoda.dev@gmail.com>
2024-08-24 14:45:12 -0400

    Input usuario

--- Practica/Program.cs
+++ Practica/Program.cs
@@ -5,14 +5,34 @@
         static void Main(string[] args)
         {
 
-            String name = "Adonis";
-            int birthday = 10;
-            Console.WriteLine("Hello, W");
+            String firstName = String.Empty;
+            String lastName = String.Empty;
+            int age = 0;
+            decimal salary = 0;
+            char gender = char.MinValue;
+            bool working = false;
 
-            String a = "Hola";
+            Console.Write("Please enter your name: ");
+            firstName = Console.ReadLine();
 
-            Console.WriteLine($"Wena {a}");
-            int age = 10;
+            Console.Write("Please enter your last name: ");
+            lastName = Console.ReadLine();
+
+            Console.Write("Please enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Please enter your salary: ");
+            salary = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Please enter your gender: ");
+            gender = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Are you working? (true or false) ");
+            working = Convert.ToBoolean(Console.ReadLine());
+
+
+            Console.WriteLine("Hello {0} {1} you're {2} years old", firstName, lastName, age);
+            Console.WriteLine("You earn {0}, you're {1} and you working status is {2}", salary, gender, working);
         }
     }
 }
