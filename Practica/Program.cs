cc529ee8f3f22ef0e7d495c818e1ef2bb250a49f
sinoda4 <sinoda.dev@gmail.com>
2024-08-24 01:12:43 -0400

    First changes

--- Practica/Program.cs
+++ Practica/Program.cs
@@ -4,11 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            String name = "Adonis";
+            int birthday = 10;
+            Console.WriteLine("Hello, W");
 
             String a = "Hola";
 
             Console.WriteLine($"Wena {a}");
+            int age = 10;
         }
     }
 }
